6e702e036aed5047989a8b47a093564955fc5379
george <geo.xiros@gmail.com>
2018-10-24 13:47:45 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            sum = sum + 1;
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
